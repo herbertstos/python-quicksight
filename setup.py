04f3d6e16ddbec38896ff9aa82024e8d20e508ad
herbert.santos <herbert.santos@gmail.com>
2023-08-08 21:21:55 -0300

    :lipstick: Added struct python

---
+++ setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup
+
+setup(
+    name='python-quicksight',
+    version='0.0.1-alpha',
+    author='',
+    author_email='',
+    packages=['pythonquicksight'],
+    description='',
+    long_description='',
+    url='',
+    project_urls={
+        'Código fonte': '',
+        'Download': ''
+    },
+    license='MIT'
+
+)
